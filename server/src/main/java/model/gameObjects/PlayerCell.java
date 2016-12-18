67ba1d49dde80b9081e69393e0044de7971e14ab
Zoeey <aisml@mail.ru>
2016-12-18 18:05:44 +0300

    new Player Cell

--- server/src/main/java/model/gameObjects/PlayerCell.java
+++ server/src/main/java/model/gameObjects/PlayerCell.java
@@ -9,18 +9,22 @@ import org.apache.logging.log4j.Logger;
  * Created by User on 10.10.2016.
  */
 
+//Игровая сущность, управляемая игроком
 public class PlayerCell extends Cell {
 
     private final int id;
+    private final String name;
 
-    public PlayerCell(int id, Location location) {
+    public PlayerCell(int id, Location location, String name) {
         super(location, GameConstants.DEFAULT_PLAYER_CELL_MASS);
         this.id = id;
+        this.name = name;
     }
 
     public int getId() {
         return id;
     }
 
+    public String getName(){return name;}
 
 }
